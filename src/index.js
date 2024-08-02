b9653df13626212586ca0084246cb8bcd8018dd5
Aurorn <aurornispro@gmail.com>
2024-08-02 18:45:08 -0400

    Added correct elements to header and style

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import "./styles/styles.css";
 import { createHeader } from "./modules/header";
 
-
 createHeader();
--- src/modules/header.js
+++ src/modules/header.js
@@ -1,40 +1,53 @@
 export const createHeader = () => {
-  const header = document.createElement('div');
-  header.className = 'header';
+  const header = document.createElement("div");
+  header.className = "header";
 
   document.body.appendChild(header);
 
-  const headLogo = document.createElement('div');
-  headLogo.className = 'head-logo';
+  const headLogo = document.createElement("div");
+  headLogo.className = "head-logo";
 
   header.appendChild(headLogo);
 
-  const headTitleContainer = document.createElement('div');
-  headTitleContainer.className = 'head-title-container';
+  const headTitleContainer = document.createElement("div");
+  headTitleContainer.className = "head-title-container";
 
   header.appendChild(headTitleContainer);
 
-  const navMenuBox = document.createElement('div');
-  navMenuBox.className = 'nav-menu-box';
+  const navMenuBox = document.createElement("div");
+  navMenuBox.className = "nav-menu-box";
 
   header.appendChild(navMenuBox);
 
-  const logoImg = document.createElement('img');
-  logoImg.className = 'logo-img';
-  logoImg.src = '#';
+  const logoImg = document.createElement("img");
+  logoImg.className = "logo-img";
+  logoImg.src = "#";
 
   headLogo.appendChild(logoImg);
 
-  const headTitle = document.createElement('h1');
-  headTitle.className = 'head-title';
-  headTitle.innerText = 'Battleship Game';
+  const headTitle = document.createElement("h1");
+  headTitle.className = "head-title";
+  headTitle.innerText = "Battleship Game";
 
   headTitleContainer.appendChild(headTitle);
 
-  const navMenu = document.createElement('ul');
-  navMenu.className = 'nav-menu';
+  const gameLink = document.createElement("a");
+  const aboutLink = document.createElement("a");
+  const gitLink = document.createElement("a");
 
-  navMenuBox.appendChild(navMenu);
+  gameLink.className = "nav-menu-link";
+  gameLink.innerText = "Game";
+  gameLink.setAttribute("href", "#");
 
+  aboutLink.className = "nav-menu-link";
+  aboutLink.innerText = "About";
+  aboutLink.setAttribute("href", "#");
 
+  gitLink.className = "nav-menu-link";
+  gitLink.innerText = "Github";
+  gitLink.setAttribute("href", "#");
+
+  navMenuBox.appendChild(gameLink);
+  navMenuBox.appendChild(aboutLink);
+  navMenuBox.appendChild(gitLink);
 };
