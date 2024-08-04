17a56374c5cdfd4b1140e4c0ea4e68478c03270a
Aurorn <aurornispro@gmail.com>
2024-08-04 17:06:32 -0400

    Test to check for correctly set length

--- src/test/ship.test.js
+++ src/test/ship.test.js
@@ -1,5 +1,11 @@
-import ship from "../modules/ship";
+import Ship from "../modules/ship";
+
 
 test("Ship is correctly called if the length is between 2 and 5", () => {
-  expect(new ship(2)).not.toBeUndefined();
+  expect(new Ship(2)).not.toBeUndefined();
+});
+
+test("Ship sets the entered private length when the entered length is within bounds", () => {
+  const ship = new Ship(2);
+  expect(ship.length).toBe(2);
 });
