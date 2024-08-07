5ce61df6a2bd10c09d166e5e53fa439b80ecafe2
Aurorn <aurornispro@gmail.com>
2024-08-07 12:42:55 -0400

    Gameboard test is called correctly

--- src/test/gameBoard.test.js
+++ src/test/gameBoard.test.js
@@ -1,3 +1,6 @@
 import GameBoard from "../modules/gameBoard";
 import Ship from "../modules/ship";
 
+test("Gameboard is called correctly", () => {
+    expect(new GameBoard()).not.toBeUndefined();
+});
