4dfb817e918cd0dfdf97a7abe0a1550f9021caec
Aurorn <aurornispro@gmail.com>
2024-08-07 12:50:43 -0400

    Correctly creates a gameboard

--- src/test/gameBoard.test.js
+++ src/test/gameBoard.test.js
@@ -4,3 +4,17 @@ import Ship from "../modules/ship";
 test("Gameboard is called correctly", () => {
     expect(new GameBoard()).not.toBeUndefined();
 });
+
+test("Create a gameboard to be used", () => {
+    const gameBoard = new GameBoard();
+    gameBoard.createBoard();
+    expect(gameBoard.board.length).toBe(10);
+    gameBoard.board.forEach((x) => {
+        expect(x.length).toBe(10);
+        x.forEach((y) => {
+            expect(y).toEqual({
+                isHit: false,
+            });
+        });
+    });
+});
