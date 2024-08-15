9d41e508d819dd988a4cd56850dbe3c879e32803
Aurorn <aurornispro@gmail.com>
2024-08-15 18:43:38 -0400

    refactoring code

--- src/modules/player.js
+++ src/modules/player.js
@@ -2,14 +2,14 @@ import Ship from "./ship";
 import Gameboard from "./gameBoard";
 
 export default class Player {
-  constructor (name) {
-    this.name = name
+  constructor(name) {
+    this.name = name;
     this.Gameboard = new Gameboard();
-    this.skiff = new Ship ("skiff", 2);
-    this.submarine = new Ship ("submarine", 3);
-    this.destroyer = new Ship ("destroyer", 3);
-    this.battleship = new Ship ("battleship", 4);
-    this.carrier = new Ship ("carrier", 5);
+    this.skiff = new Ship("skiff", 2);
+    this.submarine = new Ship("submarine", 3);
+    this.destroyer = new Ship("destroyer", 3);
+    this.battleship = new Ship("battleship", 4);
+    this.carrier = new Ship("carrier", 5);
   }
 
   startAttack(column, row, enemyBoard) {
