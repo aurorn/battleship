e7e9214a62f08f526c8876e75074048774219a4b
Aurorn <aurornispro@gmail.com>
2024-08-27 09:28:05 -0400

    Added computer function to place ships

--- src/modules/placeShips.js
+++ src/modules/placeShips.js
@@ -1,4 +1,6 @@
-function playerShipPlacement(player) {
+export default class ShipPlacement {
+
+    playerShipPlacement(player) {
     player.gameboard.placeShip(8, 7, player.Skiff);
     player.gameboard.placeShip(8, 8, player.skiff);
 
@@ -19,5 +21,30 @@ function playerShipPlacement(player) {
     player.gameboard.placeShip(1, 5, player.carrier);
     player.gameboard.placeShip(1, 6, player.carrier);
     player.gameboard.placeShip(1, 7, player.carrier);
+
+    }
+
+    computerShipPlacement(player) {
+    player.gameboard.placeShip(8, 0, player.Skiff);
+    player.gameboard.placeShip(9, 0, player.skiff);
+
+    player.gameboard.placeShip(4, 4, player.submarine);
+    player.gameboard.placeShip(4, 5, player.submarine);
+    player.gameboard.placeShip(4, 6, player.submarine);
+
+    player.gameboard.placeShip(3, 1, player.destroyer);
+    player.gameboard.placeShip(3, 1, player.destroyer);
+    player.gameboard.placeShip(3, 1, player.destroyer);
+
+    player.gameboard.placeShip(0, 0, player.battleship);
+    player.gameboard.placeShip(0, 1, player.battleship);
+    player.gameboard.placeShip(0, 2, player.battleship);
+    player.gameboard.placeShip(0, 3, player.battleship);
+
+    player.gameboard.placeShip(1, 8, player.carrier);
+    player.gameboard.placeShip(2, 8, player.carrier);
+    player.gameboard.placeShip(3, 8, player.carrier);
+    player.gameboard.placeShip(4, 8, player.carrier);
     
+    }
 }
