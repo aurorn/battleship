97e03d4fab94acfdde9ed7c0749fead16ca770bf
Aurorn <aurornispro@gmail.com>
2024-08-27 12:41:06 -0400

    Fixed naming scheme for easier understanding

--- src/modules/player.js
+++ src/modules/player.js
@@ -18,4 +18,34 @@ export default class Player {
     }
     return;
   }
+
+  startPlayerBoard() {
+    for (let i = 0; i < this.Gameboard.board.length; i++) {
+      for (let j = 0; j < this.Gameboard.board[i].length; j++) {
+        if (
+          this.Gameboard.board[i][j] !== "hit" &&
+          this.Gameboard.board[i][j] !== "miss" &&
+          this.Gameboard.board[i][j] !== ""
+        ) {
+          let square = document.getElementById(`p${j}${i}`);
+          square.textContent = "S";
+        }
+      }
+    }
+  }
+
+  startCompBoard() {
+    for (let i = 0; i < this.Gameboard.board.length; i++) {
+      for (let j = 0; j < this.Gameboard.board[i].length; j++) {
+        if (
+          this.Gameboard.board[i][j] !== "hit" &&
+          this.Gameboard.board[i][j] !== "miss" &&
+          this.Gameboard.board[i][j] !== ""
+        ) {
+          let square = document.getElementById(`o${j}${i}`);
+          square.textContent = "S";
+        }
+      }
+    }
+  }
 }
