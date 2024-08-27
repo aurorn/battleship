d064118d89978fa26bdd789a25f33225540b5640
Aurorn <aurornispro@gmail.com>
2024-08-27 11:29:15 -0400

    Added the Game loop to be called

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./styles/styles.css";
 import { createNavBar } from "./modules/navBar";
 import DOM from "./modules/DOMloader";
+import Loop from "./modules/gameLoop";
 
 createNavBar();
 DOM.gameLoad();
+Loop.gameLoop();
