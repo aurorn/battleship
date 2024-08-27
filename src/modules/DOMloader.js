0ec8559ec51177a89c23ddee7c6282a8efc49696
Aurorn <aurornispro@gmail.com>
2024-08-27 12:38:13 -0400

    Fixed naming scheme for easier understanding

--- src/modules/DOMloader.js
+++ src/modules/DOMloader.js
@@ -1,83 +1,90 @@
 export default class DOM {
-
-    static gameLoad() {
-        document.body.appendChild(this.createPlayerBoard());
-        document.body.appendChild(this.createOpponentBoard());
+  static gameLoad() {
+    const battleApp = document.querySelector(".battle-app");
+        battleApp.appendChild(this.createPlayerBoard());
+        battleApp.appendChild(this.createCompBoard());
     }
+  
 
     static createPlayerBoard() {
-        let playerBoard = document.createElement('div');
-        playerBoard.classList.add('playerBoard');
+    let playerBoard = document.createElement("div");
+    playerBoard.classList.add("playerBoard");
 
-        let rowMarkers = document.createElement('div');
-        rowMarkers.classList.add('rowMarkers');
-        for (let i = 0; i < 10; i++) {
-            let rowLabel = document.createElement('div');
-            rowLabel.classList.add('rowLabel');
-            rowLabel.textContent = String.fromCharCode(97 + i).toUpperCase();
-            rowMarkers.appendChild(rowLabel);
-        }
-        playerBoard.appendChild(rowMarkers);
-
-        let columnMarkers = document.createElement('div');
-        columnMarkers.classList.add('columnMarkers');
-        for (let i = 0; i < 10; i++) {
-            let columnLabel = document.createElement('div');
-            columnLabel.classList.add('columnLabel');
-            columnLabel.textContent = `${i + 1}`;
-            columnMarkers.appendChild(columnLabel);
-        }
-        playerBoard.appendChild(columnMarkers);
-
-        let gameSquares = document.createElement('div');
-        gameSquares.classList.add('playerGameSquares');
-        for (let i = 0; i < 100; i++) {
-            let playerSquare = document.createElement('div');
-            let formattedNum = (i).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
-            playerSquare.id = 'p' + formattedNum;
-            playerSquare.classList.add('playerSquare');
-            gameSquares.appendChild(playerSquare);
-        }
-        playerBoard.appendChild(gameSquares);
-
-        return playerBoard;
+    let rowMarkers = document.createElement("div");
+    rowMarkers.classList.add("rowMarkers");
+    for (let i = 0; i < 10; i++) {
+      let rowLabel = document.createElement("div");
+      rowLabel.classList.add("rowLabel");
+      rowLabel.textContent = String.fromCharCode(97 + i).toUpperCase();
+      rowMarkers.appendChild(rowLabel);
     }
+    playerBoard.appendChild(rowMarkers);
 
-    static createOpponentBoard() {
-        let opponentBoard = document.createElement('div');
-        opponentBoard.classList.add('opponentBoard');
-
-        let rowMarkers = document.createElement('div');
-        rowMarkers.classList.add('rowMarkers');
-        for (let i = 0; i < 10; i++) {
-            let rowLabel = document.createElement('div');
-            rowLabel.classList.add('rowLabel');
-            rowLabel.textContent = String.fromCharCode(97 + i).toUpperCase();
-            rowMarkers.appendChild(rowLabel);
-        }
-        opponentBoard.appendChild(rowMarkers);
-
-        let columnMarkers = document.createElement('div');
-        columnMarkers.classList.add('columnMarkers');
-        for (let i = 0; i < 10; i++) {
-            let columnLabel = document.createElement('div');
-            columnLabel.classList.add('columnLabel');
-            columnLabel.textContent = `${i + 1}`;
-            columnMarkers.appendChild(columnLabel);
-        }
-        opponentBoard.appendChild(columnMarkers);
-
-        let gameSquares = document.createElement('div');
-        gameSquares.classList.add('opponentGameSquares');
-        for (let i = 0; i < 100; i++) {
-            let opponentSquare = document.createElement('div');
-            let formattedNum = (i).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
-            opponentSquare.id = 'o' + formattedNum;
-            opponentSquare.classList.add('opponentSquare');
-            gameSquares.appendChild(opponentSquare);
-        }
-        opponentBoard.appendChild(gameSquares);
-
-        return opponentBoard;
+    let columnMarkers = document.createElement("div");
+    columnMarkers.classList.add("columnMarkers");
+    for (let i = 0; i < 10; i++) {
+      let columnLabel = document.createElement("div");
+      columnLabel.classList.add("columnLabel");
+      columnLabel.textContent = `${i + 1}`;
+      columnMarkers.appendChild(columnLabel);
     }
+    playerBoard.appendChild(columnMarkers);
+
+    let gameSquares = document.createElement("div");
+    gameSquares.classList.add("playerGameSquares");
+    for (let i = 0; i < 100; i++) {
+      let playerSquare = document.createElement("div");
+      let formattedNum = i.toLocaleString("en-US", {
+        minimumIntegerDigits: 2,
+        useGrouping: false,
+      });
+      playerSquare.id = "p" + formattedNum;
+      playerSquare.classList.add("playerSquare");
+      gameSquares.appendChild(playerSquare);
+    }
+    playerBoard.appendChild(gameSquares);
+
+    return playerBoard;
+  }
+
+   static createCompBoard() {
+    let compBoard = document.createElement("div");
+    compBoard.classList.add("compBoard");
+
+    let rowMarkers = document.createElement("div");
+    rowMarkers.classList.add("rowMarkers");
+    for (let i = 0; i < 10; i++) {
+      let rowLabel = document.createElement("div");
+      rowLabel.classList.add("rowLabel");
+      rowLabel.textContent = String.fromCharCode(97 + i).toUpperCase();
+      rowMarkers.appendChild(rowLabel);
+    }
+    compBoard.appendChild(rowMarkers);
+
+    let columnMarkers = document.createElement("div");
+    columnMarkers.classList.add("columnMarkers");
+    for (let i = 0; i < 10; i++) {
+      let columnLabel = document.createElement("div");
+      columnLabel.classList.add("columnLabel");
+      columnLabel.textContent = `${i + 1}`;
+      columnMarkers.appendChild(columnLabel);
+    }
+    compBoard.appendChild(columnMarkers);
+
+    let gameSquares = document.createElement("div");
+    gameSquares.classList.add("compGameSquares");
+    for (let i = 0; i < 100; i++) {
+      let compSquare = document.createElement("div");
+      let formattedNum = i.toLocaleString("en-US", {
+        minimumIntegerDigits: 2,
+        useGrouping: false,
+      });
+      compSquare.id = "o" + formattedNum;
+      compSquare.classList.add("compSquare");
+      gameSquares.appendChild(compSquare);
+    }
+    compBoard.appendChild(gameSquares);
+
+    return compBoard;
+  }
 }
