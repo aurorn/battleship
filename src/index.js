e609e1cf2c34fe66b4862f76836eb30f80573730
Aurorn <aurornispro@gmail.com>
2024-09-02 16:52:09 -0400

    Created loadUI function to load nav bar then board titles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import "./styles/styles.css";
 import { createNavBar } from "./modules/navBar";
+import { createBoardTitles } from "./modules/boardTitles";
 import DOM from "./modules/DOMloader";
 import Loop from "./modules/gameLoop";
 
+const loadUI = () => {
+    const navBar = createNavBar();
+    const boardTitlesContainer = createBoardTitles();
+    document.body.prepend(navBar, boardTitlesContainer);
+}
+
+loadUI();
 createNavBar();
+createBoardTitles();
 DOM.gameLoad();
 Loop.gameLoop();
+
+
