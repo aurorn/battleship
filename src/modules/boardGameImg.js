b9583680c7bbd0c4b0eaf1443f12418bf8e43ffc
Aurorn <aurornispro@gmail.com>
2024-09-23 18:01:09 -0400

    removed commented code

--- src/modules/boardGameImg.js
+++ src/modules/boardGameImg.js
@@ -6,11 +6,3 @@ function importImg(r) {
 
 export const boardImgs = () => importImg(require.context('../images/ships', false, /\.svg$/));
 
-/*export const boardImgs = async () => {
-    const imgs = {};
-    const req = await import.meta.glob('../images/ships/*.svg');
-    for (const path in req) {
-      imgs[path.replace('../images/ships/', '')] = req[path];
-    }
-    return imgs;
-  };*/
