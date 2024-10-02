5c61ed47fd18adabfb122e9272bdb4fa4c64e0e0
Aurorn <aurornispro@gmail.com>
2024-10-02 13:14:32 -0400

    updated imports for alert screens and added localstorage

--- src/modules/initLoad.js
+++ src/modules/initLoad.js
@@ -1,10 +1,10 @@
 import { gameLoad } from "./DOMloader";
 import { gameLoop } from "./gameLoop";
-import { newGame } from "./startScreen";
+import { newGame, customAlerts } from "./alertScreens";
 
 function initLoad() {
-  
   document.body.appendChild(newGame());
+  document.body.appendChild(customAlerts());
 
   let startGame = document.querySelector(".startGame");
   startGame.addEventListener("click", startGameHandler);
@@ -18,18 +18,17 @@ function initLoad() {
 
 function startGameHandler() {
   let playerName = document.querySelector("#userPlayerName").value;
-    if (playerName === "") {
-      playerName = "User Player";
-    }
-    let newGameSettings = document.querySelector(".newGameSettings");
-    document.body.removeChild(newGameSettings);
-    let showApp = document.querySelector(".battle-app");
-    showApp.style.display = 'grid';
+  if (playerName === "") {
+    playerName = "User Player";
+  }
+  window.localStorage.setItem('battleshipPlayerName', `${playerName}`);
+  let newGameSettings = document.querySelector(".newGameSettings");
+  document.body.removeChild(newGameSettings);
+  let showApp = document.querySelector(".battle-app");
+  showApp.style.display = "grid";
 
-
-    gameLoad(playerName);
-    gameLoop(playerName);
+  gameLoad(playerName);
+  gameLoop(playerName);
 }
-
 
 export { initLoad };
