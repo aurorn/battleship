618a0be1239ac37e34a560ddaf3df9fd961bd0cd
Aurorn <aurornispro@gmail.com>
2024-10-02 13:16:24 -0400

    Added correct imports to be loaded

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import "./styles/styles.css";
 import { createNavBar } from "./modules/navBar";
-import { createBoardTitles } from "./modules/boardTitles";
-import DOM from "./modules/DOMloader";
-import Loop from "./modules/gameLoop";
+import { initLoad } from "./modules/initLoad";
 
 const loadUI = () => {
-    const navBar = createNavBar();
-    const boardTitlesContainer = createBoardTitles();
-    document.body.prepend(navBar, boardTitlesContainer);
-}
+  const navBar = createNavBar();
+  document.body.prepend(navBar);
+};
 
 loadUI();
-createNavBar();
-createBoardTitles();
-DOM.gameLoad();
-Loop.gameLoop();
-
-
+initLoad();
